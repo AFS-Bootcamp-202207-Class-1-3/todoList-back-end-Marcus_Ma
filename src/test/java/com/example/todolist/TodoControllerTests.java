09d12204bd7841bf39baf1a747718ef8d2a9d1fa
monsterleon <1115209655@qq.com>
2022-08-04 17:04:48 +0800

    test: implement updateTodoDone

--- src/test/java/com/example/todolist/TodoControllerTests.java
+++ src/test/java/com/example/todolist/TodoControllerTests.java
@@ -1,6 +1,8 @@
 package com.example.todolist;
 
+import com.example.todolist.dto.TodoRequest;
 import com.example.todolist.entity.Todo;
+import com.example.todolist.mapper.TodoMapper;
 import com.example.todolist.repository.JpaTodoRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
@@ -29,7 +31,8 @@ class TodoControllerTests {
     MockMvc client;
     @Autowired
     JpaTodoRepository todoRepository;
-
+    @Autowired
+    TodoMapper todoMapper;
     @BeforeEach
     public void cleanAll(){
         todoRepository.deleteAll();
@@ -53,10 +56,10 @@ class TodoControllerTests {
     @Test
     void should_return_Todo_when_addNewTodo_given_NewTodo() throws Exception {
         // given
-        Todo todo1 = new Todo();
-        todo1.setContext("test3");
+        TodoRequest todoRequest = new TodoRequest();
+        todoRequest.setContext("test3");
         ObjectMapper objectMapper = new ObjectMapper();
-        String requestJson = objectMapper.writeValueAsString(todo1);
+        String requestJson = objectMapper.writeValueAsString(todoRequest);
         // when then
         client.perform(MockMvcRequestBuilders.post("/todos")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -70,5 +73,22 @@ class TodoControllerTests {
         assertThat(allTodos, hasSize(1));
         assertThat(allTodos.get(0).getContext(), equalTo("test3"));
     }
-
+    @Test
+    void should_return_rightTodo_when_updateTodoDone_given_Done() throws Exception {
+        // given
+        Todo todo =new Todo(1,"test4",false);
+        Todo oldTodo = todoRepository.save(todo);
+        TodoRequest todoRequest = new TodoRequest();
+        todoRequest.setDone(true);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestJson = objectMapper.writeValueAsString(todoRequest);
+        // when then
+        client.perform(MockMvcRequestBuilders.put("/todos/{id}",oldTodo.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(oldTodo.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.context").value("test4"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.done").value(true));
+    }
 }
