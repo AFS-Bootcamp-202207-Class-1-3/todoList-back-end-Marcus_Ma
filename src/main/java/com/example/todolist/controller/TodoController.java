50259db17b6ac9a794448f10076c3675f4a0c4d0
monsterleon <1115209655@qq.com>
2022-08-04 13:32:12 +0800

    feat: back-end getAllTodoList

--- src/main/java/com/example/todolist/controller/TodoController.java
+++ src/main/java/com/example/todolist/controller/TodoController.java
@@ -1,4 +1,26 @@
 package com.example.todolist.controller;
 
+import com.example.todolist.entity.Todo;
+import com.example.todolist.service.TodoService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("todos")
+@CrossOrigin("*")
 public class TodoController {
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping
+    public List<Todo> getAllTodoList(){
+        return todoService.finAll();
+    }
 }
---
+++ src/main/java/com/example/todolist/service/TodoService.java
@@ -0,0 +1,19 @@
+package com.example.todolist.service;
+
+import com.example.todolist.entity.Todo;
+import com.example.todolist.repository.JpaTodoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+    JpaTodoRepository jpaTodoRepository;
+
+    public TodoService(JpaTodoRepository jpaTodoRepository) {
+        this.jpaTodoRepository = jpaTodoRepository;
+    }
+    public List<Todo> finAll(){
+        return jpaTodoRepository.findAll();
+    }
+}
